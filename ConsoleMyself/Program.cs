a3b8e2459e3d80677952b7822a7b38ece9ad5e98
abhishekj <abhishekj@ABHISHEKJPC>
2019-01-29 12:57:31 +0530

    Reverted

--- ConsoleMyself/Program.cs
+++ ConsoleMyself/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleMyself
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!");
+            Console.WriteLine("Hello World!");
 
             Console.ReadLine();
         }
